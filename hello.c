ee9f30a2c275704f0770c7c2ede63c170bbc43c1
CarolynCumby <ccumby2@huskers.unl.edu>
2026-08-27 10:07:01 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Carolyn Cumby
+ *       ccumby2@huskers.unl.edu
+ * Date: 2026/08/27
+ *
+ * COME BACK TO DESC
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Carolyn\n");
+
+    return 0;
+}
